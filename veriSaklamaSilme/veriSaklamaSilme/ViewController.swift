ca4c575f72bfaabeae3d079c1027005bf13b3dd1
yaseminsalann <yaseminsalann@gmail.com>
2025-01-20 16:55:52 +0300

    Update ViewController.swift
    UserDefaults ile veri saklama işlemi eklendi.

--- veriSaklamaSilme/veriSaklamaSilme/ViewController.swift
+++ veriSaklamaSilme/veriSaklamaSilme/ViewController.swift
@@ -21,10 +21,38 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //UserDefaults ile hafızada tutulan verilerin çekilmesi işlemi yapılıyor.
+        let savedName = UserDefaults.standard.object(forKey: "name")
+        let savedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        //savedName değerini object olarak aldığımız için herhangi bir türde olabilir bu sebeple aşağıdaki gibi string olarak çevirmemiz gerekiyor.
+        //Casting - as? vs. as!
+        
+        if let newName = savedName as? String{
+            //savedName değerini string olarak kaydedebilirsen eğer newName olarak değişken oluştur demek
+            nameLabel.text = "Name: \(newName)"
+            
+        }
+        if let newBirthday = savedBirthday as? String{
+            //savedBirthday değerini string olarak kaydedebilirsen eğer newBirthday olarak değişken oluştur demek
+            birtdayLabel.text = "Birthday: \(newBirthday)"
+        }
+
+        
+        
+        
     }
 
     @IBAction func saveClicked(_ sender: Any) {
+        //set içinde kaydetmek istediğin değerin ne olduğunu ve kaydetmek istediğin anahtar kelime ne diye sorulur
+        //forkey içinde tuttuğum değer bize hatırlamamızda yardıcı olucak herhangi bir  anahtar kelime olabilir ve daha sonra çağırdığımızda bu anahtar kelime ile çağıracağız.
+        UserDefaults.standard.set(nameTextField.text!,forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text!,forKey: "birthday")
+        
+        // eskiden synchronize işlemi yapılmak istendiğinde aşağıdaki kod bloğu kullanılıyordu ama artık buna geek yok otomatik olarak gerçekleştirilmektedir.
+       // UserDefaults.standard.synchronize()
+        
+
         nameLabel.text = "Name: \(nameTextField.text!)"
         birtdayLabel.text = "Birthday: \(birthdayTextField.text!)"
     }
