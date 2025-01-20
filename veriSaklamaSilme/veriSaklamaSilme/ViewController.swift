0f1b17cdece070638cf081f5e396a1bc88874668
yaseminsalann <yaseminsalann@gmail.com>
2025-01-20 15:18:04 +0300

    veriSaklamaSilme
    ilk olarak veri saklama işlemi yapılacakdır

---
+++ veriSaklamaSilme/veriSaklamaSilme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  veriSaklamaSilme
+//
+//  Created by Yasemin salan on 20.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ veriSaklamaSilme/veriSaklamaSilmeTests/veriSaklamaSilmeTests.swift
@@ -0,0 +1,17 @@
+//
+//  veriSaklamaSilmeTests.swift
+//  veriSaklamaSilmeTests
+//
+//  Created by Yasemin salan on 20.01.2025.
+//
+
+import Testing
+@testable import veriSaklamaSilme
+
+struct veriSaklamaSilmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
