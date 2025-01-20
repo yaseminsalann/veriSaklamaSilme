66512b21fce1b40382376e724646ae3d837a0b92
yaseminsalann <yaseminsalann@gmail.com>
2025-01-20 15:38:37 +0300

    verisaklamasilme
    ilk olarak verilerin kullanıcıdan alınmasını sağlayan basit tasarımlı bir arayüz eklendi.

--- veriSaklamaSilme/veriSaklamaSilme/ViewController.swift
+++ veriSaklamaSilme/veriSaklamaSilme/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var birthdayTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birtdayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveClicked(_ sender: Any) {
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birtdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+    }
+    
 }
 
